e9c4b72eb728119b7dad3fb53756eec43799915f
bot <x@x.x>
2025-02-14 14:11:06 +0000

    remove some of ui stubs

--- client/src/components/Dashboard.vue
+++ client/src/components/Dashboard.vue
@@ -40,9 +40,7 @@ const handleTransaction = (transaction: Transaction) =>
                 <div>
                     <TitleLabel text="INSTRUCTIONS" />
                     <div class="flex flex-wrap gap-1">
-                        <div
-                            class="px-2 py-1 text-sm bg-gray-800 border border-gray-700 rounded-md hover:bg-gray-700 transition-colors">
-                            AI-related</div>
+
                     </div>
                 </div>
                 <div>
@@ -56,38 +54,18 @@ const handleTransaction = (transaction: Transaction) =>
                 <div>
                     <TitleLabel text="AGENTS" class="mt-4" />
                     <div class="flex flex-wrap gap-2">
-                        <div class="bg-green-500 w-5 h-5 rounded-sm"></div>
                     </div>
                 </div>
                 <div>
                     <TitleLabel text="ALGORITHMS" class="mt-4" />
                     <div class="flex flex-wrap gap-2">
-                        <div
-                            class="px-2 py-1 text-sm  bg-gray-800 border border-gray-700 rounded-md hover:bg-gray-700 transition-colors">
-                            Uniqueness</div>
-                        <div
-                            class="px-2 py-1 text-sm  bg-gray-800 border border-gray-700 rounded-md hover:bg-gray-700 transition-colors">
-                            Rugcheck</div>
-                        <div
-                            class="px-2 py-1 text-sm  bg-gray-800 border border-gray-700 rounded-md hover:bg-gray-700 transition-colors">
-                            Website presence</div>
-                        <div
-                            class="px-2 py-1 text-sm  bg-gray-800 border border-gray-700 rounded-md hover:bg-gray-700 transition-colors">
-                            X or Telegram presence</div>
+
                     </div>
                 </div>
                 <div>
                     <TitleLabel text="FUNCTIONS" class="mt-4" />
                     <div class="flex flex-wrap gap-2">
-                        <div
-                            class="px-2 py-1 text-sm  bg-gray-800 border border-gray-700 rounded-md hover:bg-gray-700 transition-colors">
-                            Browser</div>
-                        <div
-                            class="px-2 py-1 text-sm  bg-gray-800 border border-gray-700 rounded-md hover:bg-gray-700 transition-colors">
-                            Crawler</div>
-                        <div
-                            class="px-2 py-1 text-sm  bg-gray-800 border border-gray-700 rounded-md hover:bg-gray-700 transition-colors">
-                            Buy</div>
+
                     </div>
                 </div>
             </div>
